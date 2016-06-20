33083015bf46aa1b232306d94c3fbee888a3e2ec
kamimrcht <camille.marchet@inria.fr>
2016-06-20 23:38:46 +0200

    cleaned the comments

--- src/compaction.cpp
+++ src/compaction.cpp
@@ -107,12 +107,8 @@ void appendListReadsToRemovePref(const vector<edge>& vectL, const vector<edge>& 
 	vector<edge> vectResult;
 	uint i(0);
 	while (i < vectL.size()){
-		//~ cout << "size pour mon mari "<< vectL[i].sequence<<endl;
-		//~ cout << "size du sex " << seqsToRemove.size() << endl;
 		if (seqsToRemove.unordered_set::count(vectL[i].sequence)){
-			//~ cout << "1" << endl;
 			if (vectL[i].canonical == true){  // prefix
-				//~ cout << "11" << endl;
 				readsToRemove.insert(vectL[i].index);
 			}
 		}
@@ -121,7 +117,6 @@ void appendListReadsToRemovePref(const vector<edge>& vectL, const vector<edge>& 
 	uint j(0);
 	while (j < vectR.size()){
 		if (seqsToRemove.unordered_set::count(vectR[j].sequence)){
-			//~ cout << "2" << endl;
 			if (vectR[j].canonical == false){  // prefix
 				readsToRemove.insert(vectR[j].index);
 			}
@@ -133,14 +128,10 @@ void appendListReadsToRemovePref(const vector<edge>& vectL, const vector<edge>& 
 
 /* from sequences spotted, get reads' index for reads we do not want the suffix to be present in the next pass */ 
 void appendListReadsToRemoveSuff(const vector<edge>& vectL, const vector<edge>& vectR, const unordered_set<string>& seqsToRemove, unordered_set<int>& readsToRemove){
-	//~ cout << "rmv sff" << endl;
 	vector<edge> vectResult;
 	uint i(0);
 	while (i < vectL.size()){
-		//~ cout << "size pour mon mari "<< vectL[i].sequence<<endl;
-		//~ cout << "size du sex " << seqsToRemove.size() << endl;
 		if (seqsToRemove.unordered_set::count(vectL[i].sequence)){
-			//~ cout << "3" << endl;
 			if (vectL[i].canonical == false){  // suffix
 				readsToRemove.insert(vectL[i].index);
 			}
@@ -150,7 +141,6 @@ void appendListReadsToRemoveSuff(const vector<edge>& vectL, const vector<edge>& 
 	uint j(0);
 	while (j < vectR.size()){
 		if (seqsToRemove.unordered_set::count(vectR[j].sequence)){
-			//~ cout << "4" << endl;
 			if (vectR[j].canonical == true){  // suffix
 				readsToRemove.insert(vectR[j].index);
 			}
@@ -182,16 +172,13 @@ string compaction(const readStruct& seq1, const readStruct& seq2, uint k, unorde
 		if (not b){ // read's prefix will become its suffix and read's suffix will become its prefix
 			string compactedToReturn(getCanonical(compacted));
 			if (readsToRemovePref.unordered_set::count(seq1.index)){
-				//~ cout <<  "AAAAAAAAAAAA" << endl;
 				readsToRemoveSuff.insert(seq1.index);
 			}
 			if (readsToRemoveSuff.unordered_set::count(seq2.index)){
-				//~ cout << "BBBBBBBBBBBBBB" << endl;
 				readsToRemovePref.insert(seq1.index);
 			}
 			return compactedToReturn;
 		} else { // register read's new suffix (the sequence 2's suffix)
-			//~ cout << "CCCCCCCCCCCC" << endl;
 			if (readsToRemoveSuff.unordered_set::count(seq2.index)){
 				readsToRemoveSuff.insert(seq1.index);
 			}
@@ -210,7 +197,6 @@ string compaction(const readStruct& seq1, const readStruct& seq2, uint k, unorde
 			}
 			return compactedToReturn;
 		} else { // register read's new prefix
-			//~ readsToRemovePref.erase(seq2.index);
 			return compacted;
 		}
 	} else if (rEnd1.sequence == end2.sequence) { //  overlap FR
@@ -233,8 +219,6 @@ string compaction(const readStruct& seq1, const readStruct& seq2, uint k, unorde
 		}
 	} else if (beg1.sequence == rBeg2.sequence){ //  overlap RF
 		string compacted(rSeq2 + seq1.sequence.substr(k));
-		/* tests */
-		//~ string compactedToReturn(getCanonical(compacted));
 		bool b(isCanonical(compacted));
 		if (not b){ // new read's suffix is read 2's suffix, new read's prefix is read 1's suffix
 			string compactedToReturn(getCanonical(compacted));
@@ -300,12 +284,8 @@ void parseVector(vector<edge>& left, vector<edge>& right, vector<readStruct>& re
 	} else {
 		rightSingles = right;
 	}
-	//~ cout << "SIZE " << seqsToRemoveInPref.size() << endl;
-	//~ cout << "SIZE " <<  seqsToRemoveInSuff.size() << endl;
 	appendListReadsToRemovePref(leftSingles, rightSingles, seqsToRemoveInPref, readsToRemovePref);
 	appendListReadsToRemoveSuff(leftSingles, rightSingles, seqsToRemoveInSuff, readsToRemoveSuff);
-	//~ cout << "SIZE " << readsToRemovePref.size() << endl;
-	//~ cout << "SIZE " <<  readsToRemoveSuff.size() << endl;
 	uint indexL(0),indexR(0);
 	while (indexL < leftSingles.size() and indexR < rightSingles.size()){
 		if (leftSingles[indexL].sequence == rightSingles[indexR].sequence){
@@ -323,8 +303,6 @@ void parseVector(vector<edge>& left, vector<edge>& right, vector<readStruct>& re
 			}
 		}
 	}
-	//~ cout << "SIZE " << readsToRemovePref.size() << endl;
-	//~ cout << "SIZE " <<  readsToRemoveSuff.size() << endl;
 }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -29,12 +29,7 @@ int main(int argc, char ** argv){
 		//~ randGenome(1000000);
 		//genome from ref
 		//~ perfectsReadsFromRef("simulGenome",100,1000*1000);
-		//~ perfectsReadsFromRef("../ecoliref.fa",100,4*10*1000*1000);
-		//~ perfectsReadsFromRef("../lambda_virus.fa", 100, 2.5*1000);
-		//~ perfectsReadsFromRef("test_2SNP.fa", 10, 100);
 		//~ mutateReadsFromRef("../lambda_virus.fa", 100, 2*1000);
-		//~ mutateReadsFromRef("../ecoliref.fa", 100, 5*1000*1000);
-		//~ perfectsReadsFromRef("simulGenome",200,1*1000*1000);
 	} else {
 		bool graph(false);
 		if (argc == 4){
@@ -50,12 +45,7 @@ int main(int argc, char ** argv){
 		uint nbBuckets(1);
 		vector <ofstream> outFiles(nbBuckets);
 		string titre("out_k"+to_string(k)+ "_" + getFileName(fileName) +".fa");
-		//~ cout<<titre<<endl;
 		ofstream out(titre);
-		//~ out<<"lol"<<endl;
-		//~ cout<<"lol"<<endl;
-		//~ cout << getFileName(fileName);
-	//~ cin.get();
 
 		string sequence,sequence2;
 		vector <readStruct> sequencesVec;
@@ -66,7 +56,6 @@ int main(int argc, char ** argv){
 		setreadStructsIndex(sequencesVec);
 		auto end=chrono::system_clock::now();auto waitedFor=end-startChrono;
 		cout<<"Init took : "<<(chrono::duration_cast<chrono::seconds>(waitedFor).count())<<" sec"<<endl;
-		//~ uint size(sequencesVec.size());
 		unordered_set<string> seqsToRemoveInSuff;
 		unordered_set<string> seqsToRemoveInPref;
 		unordered_set<int> readsToRemovePref;
@@ -88,127 +77,16 @@ int main(int argc, char ** argv){
 				}
 				auto endFor=chrono::system_clock::now();auto wFor=endFor-startfor;
 				auto startparse=chrono::system_clock::now();
-				/* debug */
-				//~ for (uint i(0); i<left.size(); ++i){
-					//~ cout << "L "<< left[i].sequence << endl;
-				//~ }
-				//~ for (uint i(0); i<right.size(); ++i){
-					//~ cout << "R "<< right[i].sequence << endl;
-				//~ }
-				//~ if (k == 83){
-					//~ for (auto iter = readsToRemovePref.begin(); iter != readsToRemovePref.end(); ++ iter){
-						//~ if (*iter == 6 or *iter == 8){
-							//~ cout << "no compaction with prefix of " << *iter << endl;
-							//~ cout <<sequencesVec[*iter].sequence << endl;
-						//~ }
-						//~ cout << "not allowed pref " <<  sequencesVec[*iter].index << endl;
-					//~ }
-						
-					//~ for (auto iter = readsToRemoveSuff.begin(); iter != readsToRemoveSuff.end(); ++ iter){
-						//~ if (*iter == 6 or *iter == 8){
-							//~ cout << "no compaction with suffix of " << *iter << endl;
-							//~ cout <<sequencesVec[*iter].sequence << endl;
-						//~ }
-						//~ cout << "not allowed suff " << sequencesVec[*iter].index << endl;
-					//~ }
-						
-
-					//~ for (uint i(0); i < left.size(); ++i){
-						//~ cout << "left" << left[i].index << left[i].sequence << endl;
-					//~ }
-					//~ for (uint i(0); i < right.size(); ++i){
-						//~ cout << "right" << right[i].index << right[i].sequence << endl;
-					//~ }
-					//~ for (uint i(0); i < sequencesVec.size(); ++i){
-						//~ if (sequencesVec[i].index == 83){
-							//~ cout << k << " " << sequencesVec[i].index << " "<< sequencesVec[i].sequence << " "<< sequencesVec[i].sequence.size() << endl;
-						//~ }
-						//~ if (sequencesVec[i].index == 26){
-							//~ cout << k << " " << sequencesVec[i].index << " "<< sequencesVec[i].sequence << " "<< sequencesVec[i].sequence.size() << endl;
-						//~ }
-						//~ if (sequencesVec[i].index == 52){
-							//~ cout << k << " " << sequencesVec[i].index << " "<< sequencesVec[i].sequence << " "<< sequencesVec[i].sequence.size() << endl;
-						//~ }
-					//~ }
-				//~ }
-				//~ if (k == 93){
-					//~ for (uint i(0); i < sequencesVec.size(); ++i){
-						//~ if (sequencesVec[i].index == 83){
-							//~ cout << k << " " <<sequencesVec[i].index << " "<< sequencesVec[i].sequence <<" "<< sequencesVec[i].sequence.size() <<endl;
-						//~ }
-						//~ if (sequencesVec[i].index == 26){
-							//~ cout << k << " " <<sequencesVec[i].index << " "<< sequencesVec[i].sequence <<" "<< sequencesVec[i].sequence.size() <<endl;
-						//~ }
-						//~ if (sequencesVec[i].index == 52){
-							//~ cout << k << " " << sequencesVec[i].index << " "<< sequencesVec[i].sequence << " "<< sequencesVec[i].sequence.size() << endl;
-						//~ }
-					//~ }
-				//~ }
 				if (graph){
 					sequences2dot(sequencesVec, k, colorNodePref, colorNodeSuff, sizesNode);
 					system("dot -Tpng out.dot > output.png");
 					cin.get();
 				}
 
-				/* debug */
 				parseVector(left, right, sequencesVec, k, seqsToRemoveInSuff, seqsToRemoveInPref,  readsToRemovePref, readsToRemoveSuff);
 				auto endparse=chrono::system_clock::now();auto wParse=endparse-startparse;
 				auto end=chrono::system_clock::now();auto waitedFor=end-startChrono;
 				cout<<"k: "<<k<<": left.size "<<left.size()<<" right.size "<<right.size()<<" Step took : "<<(chrono::duration_cast<chrono::seconds>(waitedFor).count())<<" sec "<<endl;
-				/* debug */
-				//~ ifstream readStructFile2(fileName);
-				//~ unordered_set <string> initSet;
-				//~ unordered_map <string, uint> finalSet;
-				//~ uint inserted(0);
-				//~ while (not readStructFile2.eof()){
-					//~ string seq;
-					//~ getline(readStructFile2, seq);
-					//~ getline(readStructFile2, seq);
-					//~ string seqC(getCanonical(seq));
-					//~ if (not seqC.empty()){
-						//~ initSet.insert(seqC);
-						//~ ++inserted;
-					//~ }
-				//~ }
-				//~ uint rfound(0);
-				//~ for (uint i(0); i < sequencesVec.size(); ++i){
-					//~ if (not sequencesVec[i].sequence.empty()){
-						//~ uint w(0);
-						//~ do{
-							//~ readStruct read;
-							//~ read.index = sequencesVec[i].index;
-							//~ read.sequence = getCanonical(sequencesVec[i].sequence.substr(w,100));
-							//~ ++w;
-							//~ if (not read.sequence.empty()){
-								//~ auto readFound = initSet.find(read.sequence);
-								//~ if (readFound == initSet.end() ){
-								//~ } else {
-									//~ ++rfound;
-									//~ if (finalSet.count(read.sequence)){
-										//~ finalSet[read.sequence] += 1;
-											//~ cout << read.index << " REPETE: " <<read.sequence <<  " at "<< k <<  endl;
-									//~ } else {
-										//~ finalSet.insert({read.sequence,1});
-									//~ }
-								//~ }
-							//~ }
-
-						//~ } while(w < sequencesVec[i].sequence.size());
-					//~ }
-				//~ }
-				//~ if ( initSet.size() == finalSet.size()){
-					//~ cout << "*** debug *** all reads in unitigs: ok" << endl;
-					//~ cout <<  "sizes of sets: " << initSet.size() << " " << finalSet.size() << endl;
-					//~ cout <<  "inserted elements: " <<inserted << " " << rfound << endl;
-				//~ } else {
-					//~ cout << "size real reads set: " <<  size << endl;
-					//~ cout <<  "sizes of sets: " << initSet.size() << " " << finalSet.size() << endl;
-					//~ cout <<  "inserted elements: " <<inserted << " " << rfound << endl;
-				//~ }
-				/* end debug*/
-
-				
-
 				--k;				
 			} while (k>2);
 			if (graph){
